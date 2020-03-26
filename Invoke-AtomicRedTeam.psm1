b9ef2db77636ffedf070e83b70499dedcc421154
clr2of8 <clr2of8@gmail.com>
2020-03-26 11:45:55 -0600

    test

--- Invoke-AtomicRedTeam.psm1
+++ Invoke-AtomicRedTeam.psm1
@@ -1,15 +1,15 @@
 #requires -Version 3.0
 
 #Get public and private function definition files.
-$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -Exclude 'New-Atomic.ps1' -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -Exclude 'AtomicClassSchema.ps1' -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse  -ErrorAction SilentlyContinue )
 
-. "C:\AtomicRedTeam\invoke-atomicredteam\Public\New-Atomic.ps1"
+. "C:\AtomicRedTeam\invoke-atomicredteam\Private\AtomicClassSchema.ps1"
 
 #Dot source the files
 Foreach ($import in @($Public + $Private)) {
     Try {
-        #. $import.fullname
+        . $import.fullname
     }
     Catch {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
