c4cd3674868362a1047708368ad576cd95311610
clr2of8 <clr2of8@gmail.com>
2020-03-26 12:58:55 -0600

    test

--- Invoke-AtomicRedTeam.psm1
+++ Invoke-AtomicRedTeam.psm1
@@ -1,15 +1,17 @@
 #requires -Version 3.0
 
+Write-Host -ForegroundColor cyan "Calling Invoke-AtomicRedTeam.psm1"
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse  -ErrorAction SilentlyContinue )
 
-. "C:\AtomicRedTeam\invoke-atomicredteam\Private\AtomicClassSchema.ps1"
+#. "C:\AtomicRedTeam\invoke-atomicredteam\Private\AtomicClassSchema.ps1"
 
 #Dot source the files
 Foreach ($import in @($Public + $Private)) {
     Try {
         . $import.fullname
+        write-host $import
     }
     Catch {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
