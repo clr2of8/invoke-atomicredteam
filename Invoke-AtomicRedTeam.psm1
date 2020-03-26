81348486255e2dc162e05ee8b2317d97c3159241
clr2of8 <clr2of8@gmail.com>
2020-03-26 11:16:08 -0600

    test

--- Invoke-AtomicRedTeam.psm1
+++ Invoke-AtomicRedTeam.psm1
@@ -4,7 +4,7 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -Exclude 'New-Atomic.ps1' -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -Exclude 'AtomicClassSchema.ps1' -ErrorAction SilentlyContinue )
 
-# . "C:\AtomicRedTeam\invoke-atomicredteam\Public\New-Atomic.ps1"
+. "C:\AtomicRedTeam\invoke-atomicredteam\Public\New-Atomic.ps1"
 
 #Dot source the files
 Foreach ($import in @($Public + $Private)) {
