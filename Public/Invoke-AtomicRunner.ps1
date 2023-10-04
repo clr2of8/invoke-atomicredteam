04b0922169576341b9e2758cd13e51cbf2993883
clr2of8 <clr2of8@gmail.com>
2023-10-04 08:32:07 -0600

    add rename retries

--- Public/Invoke-AtomicRunner.ps1
+++ Public/Invoke-AtomicRunner.ps1
@@ -124,106 +124,106 @@ function Invoke-AtomicRunner {
                     LogRunnerMsg "uh oh, still haven't restarted - should never get to here"
                     exit
                 }
-            
             }
+        }
         
-            function Get-TimingVariable ($sched) {
-                $atcount = $sched.Count
-                if ($null -eq $atcount) { $atcount = 1 }
-                $scheduleTimeSpanSeconds = $artConfig.scheduleTimeSpan.TotalSeconds
-                $secondsForAllTestsToComplete = $scheduleTimeSpanSeconds
-                $sleeptime = ($secondsForAllTestsToComplete / $atcount) - 120 - $artConfig.kickOffDelay.TotalSeconds # 1 minute for restart and 1 minute delay for scheduled task and an optional kickoff delay
-                if ($sleeptime -lt 120) { $sleeptime = 120 } # minimum 2 minute sleep time
-                return $sleeptime
-            }
+        function Get-TimingVariable ($sched) {
+            $atcount = $sched.Count
+            if ($null -eq $atcount) { $atcount = 1 }
+            $scheduleTimeSpanSeconds = $artConfig.scheduleTimeSpan.TotalSeconds
+            $secondsForAllTestsToComplete = $scheduleTimeSpanSeconds
+            $sleeptime = ($secondsForAllTestsToComplete / $atcount) - 120 - $artConfig.kickOffDelay.TotalSeconds # 1 minute for restart and 1 minute delay for scheduled task and an optional kickoff delay
+            if ($sleeptime -lt 120) { $sleeptime = 120 } # minimum 2 minute sleep time
+            return $sleeptime
+        }
 
-            # Convert OtherArgs to hashtable so we can pass it through to the call to Invoke-AtomicTest
-            $htvars = @{}
-            if ($OtherArgs) {
-                $OtherArgs | ForEach-Object {
-                    if ($_ -match '^-') {
-                        #New parameter
-                        $lastvar = $_ -replace '^-'
-                        $htvars[$lastvar] = $true
-                    }
-                    else {
-                        #Value
-                        $htvars[$lastvar] = $_
-                    }
-                }
-            }
-
-            $htvars += [Hashtable]$PSBoundParameters
-            $htvars.Remove('listOfAtomics') | Out-Null
-            $htvars.Remove('OtherArgs') | Out-Null
-            $htvars.Remove('Cleanup') | Out-Null
-
-            $schedule = Get-Schedule $listOfAtomics
-            # If the schedule is empty, end process
-            if (-not $schedule) {
-                LogRunnerMsg "No test guid's or enabled tests."
-                return
-            }
-
-            # timing variables
-            $SleepTillCleanup = Get-TimingVariable $schedule
-
-            # Perform cleanup, Showdetails or Prereq stuff for all scheduled items and then exit
-            if ($Cleanup -or $ShowDetails -or $CheckPrereqs -or $ShowDetailsBrief -or $GetPrereqs -or $listOfAtomics) {
-                $schedule | ForEach-Object {
-                    Invoke-AtomicTestFromScheduleRow $_ $Cleanup
-                }
-                return
-            }
-
-            # exit if file stop.txt is found
-            If (Test-Path $artConfig.stopFile) {
-                LogRunnerMsg "exiting script because $($artConfig.stopFile) does exist"
-                Write-Host -ForegroundColor Yellow "Exiting script because $($artConfig.stopFile) does exist."; Start-Sleep 10;
-                exit
-            }
-        
-            # Find current test to run
-            $guid = Get-GuidFromHostName $artConfig.basehostname
-            if ([string]::IsNullOrWhiteSpace($guid)) {
-                LogRunnerMsg "Test Guid ($guid) was null, using next item in the schedule"
-            }
-            else {
-                if ($artConfig.verbose) { LogRunnerMsg "Found Test: $guid specified in hostname" }
-                $sp = [Collections.Generic.List[Object]]$schedule
-                $currentIndex = $sp.FindIndex( { $args[0].auto_generated_guid -eq $guid })
-                if (($null -ne $currentIndex) -and ($currentIndex -ne -1)) {
-                    $tr = $schedule[$currentIndex]
-                }
-
-                if ($null -ne $tr) {
-                    Invoke-AtomicTestFromScheduleRow $tr
-                    Write-Host -Fore cyan "Sleeping for $SleepTillCleanup seconds before cleaning up"; Start-Sleep -Seconds $SleepTillCleanup
-                
-                    # Cleanup after running test
-                    Invoke-AtomicTestFromScheduleRow $tr $true
+        # Convert OtherArgs to hashtable so we can pass it through to the call to Invoke-AtomicTest
+        $htvars = @{}
+        if ($OtherArgs) {
+            $OtherArgs | ForEach-Object {
+                if ($_ -match '^-') {
+                    #New parameter
+                    $lastvar = $_ -replace '^-'
+                    $htvars[$lastvar] = $true
                 }
                 else {
-                    LogRunnerMsg "Could not find Test: $guid in schedule. Please update schedule to run this test."
+                    #Value
+                    $htvars[$lastvar] = $_
                 }
             }
+        }
 
-            # Load next scheduled test before renaming computer
-            $nextIndex += $currentIndex + 1     
-            if ($nextIndex -ge ($schedule.count)) {
-                $tr = $schedule[0]
+        $htvars += [Hashtable]$PSBoundParameters
+        $htvars.Remove('listOfAtomics') | Out-Null
+        $htvars.Remove('OtherArgs') | Out-Null
+        $htvars.Remove('Cleanup') | Out-Null
+
+        $schedule = Get-Schedule $listOfAtomics
+        # If the schedule is empty, end process
+        if (-not $schedule) {
+            LogRunnerMsg "No test guid's or enabled tests."
+            return
+        }
+
+        # timing variables
+        $SleepTillCleanup = Get-TimingVariable $schedule
+
+        # Perform cleanup, Showdetails or Prereq stuff for all scheduled items and then exit
+        if ($Cleanup -or $ShowDetails -or $CheckPrereqs -or $ShowDetailsBrief -or $GetPrereqs -or $listOfAtomics) {
+            $schedule | ForEach-Object {
+                Invoke-AtomicTestFromScheduleRow $_ $Cleanup
+            }
+            return
+        }
+
+        # exit if file stop.txt is found
+        If (Test-Path $artConfig.stopFile) {
+            LogRunnerMsg "exiting script because $($artConfig.stopFile) does exist"
+            Write-Host -ForegroundColor Yellow "Exiting script because $($artConfig.stopFile) does exist."; Start-Sleep 10;
+            exit
+        }
+        
+        # Find current test to run
+        $guid = Get-GuidFromHostName $artConfig.basehostname
+        if ([string]::IsNullOrWhiteSpace($guid)) {
+            LogRunnerMsg "Test Guid ($guid) was null, using next item in the schedule"
+        }
+        else {
+            if ($artConfig.verbose) { LogRunnerMsg "Found Test: $guid specified in hostname" }
+            $sp = [Collections.Generic.List[Object]]$schedule
+            $currentIndex = $sp.FindIndex( { $args[0].auto_generated_guid -eq $guid })
+            if (($null -ne $currentIndex) -and ($currentIndex -ne -1)) {
+                $tr = $schedule[$currentIndex]
+            }
+
+            if ($null -ne $tr) {
+                Invoke-AtomicTestFromScheduleRow $tr
+                Write-Host -Fore cyan "Sleeping for $SleepTillCleanup seconds before cleaning up"; Start-Sleep -Seconds $SleepTillCleanup
+                
+                # Cleanup after running test
+                Invoke-AtomicTestFromScheduleRow $tr $true
             }
             else {
-                $tr = $schedule[$nextIndex]
+                LogRunnerMsg "Could not find Test: $guid in schedule. Please update schedule to run this test."
             }
-        
-            if ($null -eq $tr) { 
-                LogRunnerMsg "Could not determine the next row to execute from the schedule, Starting from 1st row"; 
-                $tr = $schedule[0] 
-            }
-
-            #Rename Computer and Restart
-            Rename-ThisComputer $tr $artConfig.basehostname
-    
         }
+
+        # Load next scheduled test before renaming computer
+        $nextIndex += $currentIndex + 1     
+        if ($nextIndex -ge ($schedule.count)) {
+            $tr = $schedule[0]
+        }
+        else {
+            $tr = $schedule[$nextIndex]
+        }
+        
+        if ($null -eq $tr) { 
+            LogRunnerMsg "Could not determine the next row to execute from the schedule, Starting from 1st row"; 
+            $tr = $schedule[0] 
+        }
+
+        #Rename Computer and Restart
+        Rename-ThisComputer $tr $artConfig.basehostname
+    
     }
+}
